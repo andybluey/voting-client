31ebc5f9f3f777bfcd4f20663d028c8a8c13bb94
Andrew Knight <andrewjaknight@gmail.com>
2016-12-08 08:55:43 +1100

    Set up Voting_spec and relevant test

--- test/test_helper.js
+++ test/test_helper.js
@@ -8,4 +8,10 @@ const win = doc.defaultView;
 global.document = doc;
 global.window = win;
 
+Object.keys(window).forEach((key) => {
+  if (!(key in global)) {
+    global[key] = window[key];
+  }
+});
+
 chai.use(chaiImmutable);
